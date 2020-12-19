158f9bef38155a4f0a8d9aed390ec780a8104ed3
Dhanush <dhanushlingeswaran@outlook.com>
2020-12-19 18:13:21 +0100

    Drawing rectangles to depict current ROI

--- Lane Detection/Lane_Detection_Prototype_I.py
+++ Lane Detection/Lane_Detection_Prototype_I.py
@@ -9,7 +9,7 @@ import cv2 as cv
 import numpy as np
 import math
 from matplotlib import pyplot as plt
-path = r'J:\\ESD Project\\test_video\\test_video_1.MOV'
+path = r'E:\\ESD_Project\\Takeover.M4v'
 
 #Method to draw the lines obtained from Hough transform
 def draw_the_lines(image,lines,roi):
@@ -22,15 +22,25 @@ def draw_the_lines(image,lines,roi):
     img =cv.addWeighted(img,0.8,blank_image,1,0.0)
     return img
 #Method to Calibrate ROI
-def Calibration(frame_cal):
-    global roi_Cal
+def Calibration(frame_cal,roi_CalFunc):
+    print(type(roi_CalFunc[0]));
+    start_point = (int(roi_CalFunc[0]),int(roi_CalFunc[1]))
+    end_point = (int(roi_CalFunc[2]),int(roi_CalFunc[3]))
+    print(start_point)
+    print(end_point)
+    color = (0, 255, 0) 
+    # Line thickness of 2 px 
+    thickness = 2
+    cv.rectangle(img=frame_cal, pt1=(int(roi_CalFunc[0]), int(roi_CalFunc[1])), pt2=((int(roi_CalFunc[0])+int(roi_CalFunc[2])),(int(roi_CalFunc[1])+int(roi_CalFunc[3]))), color=(0, 0, 255), thickness=2)
+    roi_CalOut=cv.selectROI(frame_cal)
     f= open("ROI_Cal.txt","w")
-    roi_Cal=cv.selectROI(frame_cal)
-    f.write(str(roi_Cal[0])+"\n")
-    f.write(str(roi_Cal[1])+"\n")
-    f.write(str(roi_Cal[2])+"\n")
-    f.write(str(roi_Cal[3])+"\n")
-    return roi_Cal
+    f.write(str(roi_CalOut[0])+"\n")
+    f.write(str(roi_CalOut[1])+"\n")
+    f.write(str(roi_CalOut[2])+"\n")
+    f.write(str(roi_CalOut[3])+"\n")
+    return roi_CalOut
+def elements(array):
+    return array.ndim and array.size
 #Caputuring the video
 cap = cv.VideoCapture(path)
 
@@ -38,50 +48,59 @@ cap = cv.VideoCapture(path)
 roi_Cal = [x.strip() for x in open("ROI_Cal.txt","r")]
 len_Cal = [x.strip() for x in open("Len_Cal.txt","r")]
 ang_Cal = [x.strip() for x in open("Ang_Cal.txt","r")]
-
+roi = roi_Cal
 while (cap.isOpened()):
-	print("Press 's' to enter the Calibration mode")
-	ret, frame = cap.read()
-	frame = cv.resize(frame,(480,320))
-	if cv.waitKey(10) & 0xFF == ord('s'):
-		roi = Calibration(frame)
-	roi = roi_Cal
-	#Selecting the area where the lanes are likely to be present
-	LongLines = [];
-	
-	#Obtaining the cropped frames
-	roi_cropped = frame[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
-	#converting the frames to gray scale
-	gray=cv.cvtColor(roi_cropped,cv.COLOR_BGR2GRAY)
-	
-	#Applying canny filter for edge detection with fixed thresholds
-	canny = cv.Canny(gray, threshold1=70, threshold2=175) 
+    print("Press 's' to enter the Calibration mode")
+    ret, Orgframe = cap.read()
+    frame = cv.resize(Orgframe,(480,320))
+    if cv.waitKey(10) & 0xFF == ord('s'):
+        roi = Calibration(frame,roi)
+    
+    #Selecting the area where the lanes are likely to be present
+    LongLines = [];
+    
+    #Obtaining the cropped frames
+    roi_cropped = frame[int(roi[1]):(int(roi[1])+int(roi[3])), int(roi[0]):(int(roi[0])+int(roi[2]))]
+    #converting the frames to gray scale
+    gray=cv.cvtColor(roi_cropped,cv.COLOR_BGR2GRAY)
+    
+    
+    
+    #Applying canny filter for edge detection with fixed thresholds
+    canny = cv.Canny(gray, threshold1=70, threshold2=175) 
     
     #Applying Hough Transform to obtain the straight lines
-	lines =cv.HoughLinesP (canny,
+    lines =cv.HoughLinesP (canny,
                         rho = 1,
                         theta = np.pi/180,
                         threshold = 40,
                         lines = np.array([]),
                         minLineLength = 60,
                         maxLineGap = 5)
-	
-	#Filtering out the lane lines using length and angle
-	for line in lines:
-		for x1,y1,x2,y2 in line:
-			length = math.sqrt(((x2-x1)**2)+((y2-y1)**2));
-			angle = math.degrees(math.atan((x1-x2)/(y1-y2)));
-			if(((length >= int(len_Cal[0])) or ((length <= int(len_Cal[1])) and length >= int(len_Cal[2]))) and ((angle>(int(ang_Cal[0])) and angle<(int(ang_Cal[1]))) or ((angle <(int(ang_Cal[2]))) and (angle>(int(ang_Cal[3])))))):
-				#print("angle : "+str(math.degrees(math.atan((x1-x2)/(y1-y2)))))
-				#print("length :"+str(length));
-				LongLines.append(line);
+    lines = np.array(lines)
+    
+    #Filtering out the lane lines using length and angle
+    if elements(lines) != 0:
+        for line in lines:
+            for x1,y1,x2,y2 in line:
+                length = math.sqrt(((x2-x1)**2)+((y2-y1)**2));
+                angle = math.degrees(math.atan((x1-x2)/(y1-y2)));
+                if((length >= 30)and ((angle>(int(ang_Cal[0])) and angle<(int(ang_Cal[1]))) or ((angle <(int(ang_Cal[2]))) and (angle>(int(ang_Cal[3])))))):
+                    #print("angle : "+str(math.degrees(math.atan((x1-x2)/(y1-y2)))))
+                    #print("length :"+str(length));
+                    LongLines.append(line);
+    else:
+        print("no lines");
     #print(LongLines);
-	image_lines = draw_the_lines(frame,LongLines,roi)
-	if ret == True:
-		cv.imshow("video", image_lines)
-		if cv.waitKey(10) & 0xFF == ord('q'):
-			break
-	else:
-		break
+    if (len(LongLines) != 0):
+        image_lines = draw_the_lines(frame,LongLines,roi)
+    else:
+        image_lines = frame
+    if ret == True:
+        cv.imshow("video", image_lines)
+        if cv.waitKey(10) & 0xFF == ord('q'):
+            break
+    else:
+        break
 cap.release()
 cv.destroyAllWindows()
