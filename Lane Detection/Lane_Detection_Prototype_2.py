9e50b1c84078eb4c195c35c526bad68e823c8fed
unknown <deshmukhsayli14@gmail.com>
2021-01-12 19:39:20 +0100

    test2

--- Lane Detection/Lane_Detection_Prototype_2.py
+++ Lane Detection/Lane_Detection_Prototype_2.py
@@ -13,7 +13,7 @@ from operator import itemgetter
 import shapely
 from shapely.geometry import LineString, Point
 
-path = r'/home/pi/Desktop/ESDgit/ESDproject/sample images/test_video2.mp4'
+path = r'/home/pi/Desktop/ESDgit/ESDproject/sample images/test_video4.mp4'
 
 #Method to draw the lines obtained from Hough transform
 def draw_the_lines(image,lines,roi):
